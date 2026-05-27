50f245bf24c12a260f93d2452a61c378b40443f5
Mari0shi <unavailable>
2026-05-27 10:02:26 -0400

    I left a debug feature in sorry

--- BattleMod/Lua/3-Functions/3-Player/Special Moves/Lib_ActSuperSpinJump.lua
+++ BattleMod/Lua/3-Functions/3-Player/Special Moves/Lib_ActSuperSpinJump.lua
@@ -206,33 +206,30 @@ B.Action.SuperSpinJump=function(mo,doaction)
 				local blastspeed = 4
 				local fuse = 11
 				
-				if B.PlayerButtonPressed(player, BT_SPIN, true) then
-					//Create projectile blast
+				//Create projectile blast
 
-					local step = 20
+				local step = 20
 
-					for n = 0, 360-step, step do
-						local angle = mo.angle+n*ANG1
-						local p = P_SPMAngle(mo,MT_GROUNDPOUND,angle,0)
-						if p and p.valid then
-							B.SafeRadiusIncrease(p, 48*FRACUNIT)
-							p.tics = fuse
-							p.spritexscale = FRACUNIT+(FRACUNIT/2)
-							p.spriteyscale = FRACUNIT+(FRACUNIT/2)
-							p.spriteyoffset = -FRACUNIT*19
-							p.momz = 0
-							p.z = ((P_MobjFlip(mo)==-1 and (mo.z+mo.height)) or mo.z)+(P_MobjFlip(mo)*(mo.scale))
-							p.fuse = fuse
-						end
+				for n = 0, 360-step, step do
+					local angle = mo.angle+n*ANG1
+					local p = P_SPMAngle(mo,MT_GROUNDPOUND,angle,0)
+					if p and p.valid then
+						B.SafeRadiusIncrease(p, 48*FRACUNIT)
+						p.tics = fuse
+						p.spritexscale = FRACUNIT+(FRACUNIT/2)
+						p.spriteyscale = FRACUNIT+(FRACUNIT/2)
+						p.spriteyoffset = -FRACUNIT*19
+						p.momz = 0
+						p.z = ((P_MobjFlip(mo)==-1 and (mo.z+mo.height)) or mo.z)+(P_MobjFlip(mo)*(mo.scale))
+						p.fuse = fuse
 					end
-				else
-
-					//Create damaging shockwave
-					local old_speed = mobjinfo[MT_GP_SHOCKWAVE].speed
-					mobjinfo[MT_GP_SHOCKWAVE].speed = gp_distance / mobjinfo[MT_GP_SHOCKWAVE].painchance
-					A_Shockwave(mo, MT_GP_SHOCKWAVE, 16)
-					mobjinfo[MT_GP_SHOCKWAVE].speed = old_speed
 				end
+
+				/*Create damaging shockwave
+				local old_speed = mobjinfo[MT_GP_SHOCKWAVE].speed
+				mobjinfo[MT_GP_SHOCKWAVE].speed = gp_distance / mobjinfo[MT_GP_SHOCKWAVE].painchance
+				A_Shockwave(mo, MT_GP_SHOCKWAVE, 16)
+				mobjinfo[MT_GP_SHOCKWAVE].speed = old_speed*/
 					
 				P_InstaThrust(mo,R_PointToAngle2(0,0,mo.momx,mo.momy),FixedHypot(mo.momx,mo.momy)/8)
 				B.ZLaunch(mo,reboundthrust*FRACUNIT,true)
