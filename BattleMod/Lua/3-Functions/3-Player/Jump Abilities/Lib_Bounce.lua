5c6ac07b9f8adb88144fc0daf64f48ec0a63f403
Mari0shi <unavailable>
2026-06-28 10:44:24 -0400

    Cut ONLY vertical momentum on tailbounce

--- BattleMod/Lua/3-Functions/3-Player/Jump Abilities/Lib_Bounce.lua
+++ BattleMod/Lua/3-Functions/3-Player/Jump Abilities/Lib_Bounce.lua
@@ -3,7 +3,9 @@ local B = CBW_Battle
 B.FastBounce = function(player, mo)
     if player.pflags&PF_THOKKED then return true end
     mo.state = S_PLAY_BOUNCE
-    player.pflags = $ & ~(PF_JUMPED|PF_NOJUMPDAMAGE)
-    player.pflags = $ | (PF_THOKKED|PF_BOUNCING)
+    player.pflags = $|(PF_THOKKED|PF_BOUNCING) & ~(PF_JUMPED|PF_NOJUMPDAMAGE|PF_STARTJUMP)
+    if (mo.momz*P_MobjFlip(mo)) > 0 then
+        mo.momz = $/2
+    end
     return true
 end
